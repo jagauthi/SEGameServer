0045bec96abe1a96694274b64b63f350972b0ff8
Joseph <joseph.a.gauthier@gmail.com>
2016-02-08 16:50:59 -0600

    Update ChatServer.java

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,36 +1,44 @@
-//import java.io.PrintWriter;
-//import java.net.ServerSocket;
-//import java.util.HashSet;
-//
-//public class ChatServer {
-//
-//	//Port that the clients use to connect to
-//    static final int PORT = 9001;
-//
-//    //Set of all names of clients connected
-//    static HashSet<String> names = new HashSet<String>();
-//
-//    //Set of all the writers to each of the clients
-//    static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
-//    
-//    public ChatServer(){
-//    	try {
-//			runChatServer();
-//		} 
-//    	catch (Exception e){
-//			e.printStackTrace();
-//		}
-//    }
-//    
-//    public void runChatServer() throws Exception{
-//    	ServerSocket listener = new ServerSocket(ChatServer.PORT);
-//        try{
-//            while(true){
-//                new Handler(listener.accept()).start();
-//            }
-//        } 
-//        finally {
-//            listener.close();
-//        }
-//    }
-//}
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.util.HashSet;
+
+public class ChatServer extends Thread{
+
+	//Port that the clients use to connect to
+    static final int PORT = 9001;
+
+    //Set of all names of clients connected
+    static HashSet<String> names = new HashSet<String>();
+
+    //Set of all the writers to each of the clients
+    static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+    
+    ServerSocket listener;
+    
+    public ChatServer()
+    {
+    	try {
+			listener = new ServerSocket(PORT);
+			
+	        System.out.println("The chat server is running.");
+	        InetAddress addr = InetAddress.getLocalHost();
+	        System.out.println(addr.getHostName());
+		} 
+    	catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public void run(){
+    	while(true){
+	    	try {
+				new Handler(listener.accept()).start();
+			} 
+	    	catch (IOException e) {
+				e.printStackTrace();
+			}
+    	}
+    }
+}
