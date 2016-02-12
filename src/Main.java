849fee23bf62cc711b0cb3a9153f23b856157b79
jfantinel <joseph.fantinel@gmail.com>
2016-02-11 22:51:19 -0600

    Update Main.java
    There are examples of some methods in the DatabaseConnector class that are commented out.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 public class Main {
 	
 	static DatabaseConnector db; 
@@ -10,5 +11,11 @@ public class Main {
         ChatServer cs = new ChatServer();
         cs.start();
         db = new DatabaseConnector();
+    
+        //testing code below this line...
+        //db.createAccount("otherAccountNum79","whatever", "fake@email.not", "securityQuestion1", "securityAnswer1", "securityQuestion2", "securityAnswer2");
+        //db.getAccountID("otherAccountNum79");
+       // db.createChar("WarriorPrincess", "77", "Warrior", "Female", "30", "15", "15", "5", "5", "10");
+        
     }
 }
