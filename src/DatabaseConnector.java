197d669e5d88c1dd1d9ea6965efcd3c7bced3de7
jfantinel <joseph.fantinel@gmail.com>
2016-02-11 22:58:30 -0600

    Update DatabaseConnector.java
    Added methods for getting char info for the character select screen, creating accounts, creating characters, and getting the accountID for a user account.  The create char method does NOT check that the char name is unique - it is more of an insert a row to the char table method, at the moment.

--- src/DatabaseConnector.java
+++ src/DatabaseConnector.java
@@ -37,7 +37,7 @@ public class DatabaseConnector {
 	      try
 	      {
 	         con = DriverManager.getConnection(URL, USER, PASS);
-	         stmt = con.createStatement();
+	         stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
 	      } 
 	      catch (SQLException e) {
 	         e.printStackTrace();
@@ -87,13 +87,15 @@ public class DatabaseConnector {
 	//          if numFailedAttempts >= 3 { lock AccountAndSendEmail }
 	
 	String errorCode = "";
-	String result = "";
+	//String result = "";
 	
 	
 	//we need to add code to prevent SQL injection attacks
-	String sql = "SELECT * from AccountsTable where Username = \'" + username + "\';";
+	String sql = "SELECT * from AccountTable where Username = \'" + username + "\';";
 	ResultSet rs;
+	
 	try {
+		
 		rs = stmt.executeQuery(sql);
 	
 	          
@@ -111,7 +113,7 @@ public class DatabaseConnector {
 	if ( numResults == 0 ) {
 	      System.out.println("No account found with username: " + username);
 	         rs.close();
-	      return "Nope!";
+	      return "usernameNotFound";
 	      //Call a method that suggests the player to make a new account.
 	 }
 	
@@ -119,11 +121,13 @@ public class DatabaseConnector {
 	if ( numResults > 1 ) {
 		//notify admin
 		// include username and explain the possible situation of data concurrency issues....
+		System.out.println("More than one record exist with that username.");
+		return "moreThanOneAccountFound";
 	}
 	
 	if ( numResults == 1 ) {
 		System.out.println("rs.getInt(rs.findColumn(numFailedAttempts) )  = " + rs.getInt(rs.findColumn("numFailedAttempts") ));
-		if( rs.getInt(rs.findColumn("numFailedAttempts") ) >= 3  ){
+		if( rs.getInt(rs.findColumn("numFailedAttempts") ) >= 5  ){
 			rs.updateInt("locked", 1 );
 			
 		}
@@ -131,14 +135,14 @@ public class DatabaseConnector {
 			
 		if( rs.getInt(rs.findColumn("locked") ) ==1  ){
 			//set errorCode to value for locked;
-			errorCode ="account is locked";
+			errorCode ="accountLocked";
 	         rs.close();
 			return errorCode;
 			//handler should prompt to reset password
 		}
 		if( rs.getInt(rs.findColumn("banned") ) ==1  ){
 			//set errorCode to value for locked;
-			errorCode ="account is banned";
+			errorCode ="accountBanned";
 	         rs.close();
 			return errorCode;
 			//handler should prompt to reset password
@@ -146,118 +150,293 @@ public class DatabaseConnector {
 		
 		int passwordColumnIndex = rs.findColumn("Password");;
 		if ( !password.equals( rs.getString(passwordColumnIndex)) ){
-			errorCode = "Bad Password, berate user";
+			errorCode = "incorrectPassword";
+			System.out.println(errorCode);
 			
-			rs.updateInt("numFailedAttempts", (rs.findColumn("numFailedAttempts") + 1) );
-			if( rs.findColumn("numFailedAttempts") >= 3){
+			int updatedNumFailedAttempts = (rs.getInt(rs.findColumn("numFailedAttempts")) + 1);
+			rs.updateInt("numFailedAttempts",  updatedNumFailedAttempts);
+			rs.updateRow();
+			System.out.println("number of failed attempts: " + rs.getInt(rs.findColumn("numFailedAttempts") ) );
+			if( rs.getInt(rs.findColumn("numFailedAttempts")) >= 5){
 			 //lock account and send email
 				rs.updateInt("locked", 1 );
-				errorCode = "too many failed attempts, account is now locked... shame the user"; 
-				         rs.close();
-						return errorCode;
+				errorCode = "accountLocked"; 
+				System.out.println(errorCode);
+				rs.close();
+				
+				return errorCode;
 			}
-	         rs.close();
+	        rs.close();
 			return errorCode;
 		}
 		else{
 			errorCode = "loginSuccess";
 			System.out.println(errorCode);
-	         rs.close();
+	        rs.close();
 			return errorCode;
 		}
 		
-	}
-	 } 
-    catch (SQLException e) {
-       e.printStackTrace();
-    }
-    catch (Exception e) {
-        e.printStackTrace();
-    }
-    finally
-    {  
-	
-       System.out.println("login attempt completed");
-    }
+	  }
+		} 
+	    catch (SQLException e) {
+	       e.printStackTrace();
+	    }
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    finally
+	    {  
+		
+	       System.out.println("login attempt completed");
+	    }
 	return errorCode;
 	}
-
 	
-	public String connectToDatabase()
+	public int getAccountID(String username)
 	{
-		String result = "";
-		try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-	      }
-	      catch(ClassNotFoundException ex) {
-	            System.out.println("Error: unable to load driver class!");
-	            System.exit(1);
-	      }
-	      catch(IllegalAccessException ex) {
-	            System.out.println("Error: access problem while loading!");
-	            System.exit(2);
-	      }
-	      catch(InstantiationException ex) {
-	            System.out.println("Error: unable to instantiate driver!");
-	            System.exit(3);
-	      }
-	      
-	      String URL = "jdbc:mysql://sql5.freesqldatabase.com:3306/sql5104581";
-	      String USER = "sql5104581";
-	      String PASS = "8CHGkEPagu";
-	      Connection con = null;
-	      Statement stmt = null;
-	      try
-	      {
-	         con = DriverManager.getConnection(URL, USER, PASS);
-	         stmt = con.createStatement();String sql;
-	         sql = "SELECT * from AccountsTable where Username = \'" + "jagauthi" + "\';";
-	         ResultSet rs = stmt.executeQuery(sql);
-	          
-	         //If there is no results for the entered username
-	         if (!rs.next()) {
-	           System.out.println("No account found with username: " + "jagauthi");
-	           return "Nope!";
-	           //Call a method that suggests the player to make a new account.
-	         }
-	         
-	         //Set the ResultSet counter back to the beginning of the ResultSet
-	         //This is necessary since the previous method moves the counter forward to
-	         //check if the ResultSet is empty.
-	         rs.beforeFirst();
-	         rs.next();
-	         result = rs.getString("Username");
-	         // cleaning up
-	         rs.close();
-	         stmt.close();
-	         con.close();
-	      } 
-	      catch (SQLException e) {
-	         e.printStackTrace();
-	      }
-	      catch (Exception e) {
-	          e.printStackTrace();
-	      }
-	      finally
-	      {  
-	          //closing the resources in case they weren't closed earlier
-	          try {
-	          if (stmt != null)
-	                  stmt.close();
-	          }
-	          catch (SQLException e2) {
-	             e2.printStackTrace();
-	          }
-	          try {
-	              if (con != null) {
-	                  con.close();
-	               }
-	          } 
-	          catch (SQLException e2) {
-	                 e2.printStackTrace();
-	          }
-	      }
-	      return result;
-      }
+		int accountID = -1; //This will be returned.
+		
+		ResultSet rs;
+		String sql = "SELECT * from AccountTable where Username = \'" + username + "\';";
+		try{
+			rs = stmt.executeQuery(sql);
+			rs.next();
+			accountID = rs.getInt("accountID");
+			
+		} 
+	    catch (SQLException e) {
+	       e.printStackTrace();
+	    }
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    finally
+	    {  
+		
+	       System.out.println("getAccountID completed and it is :" + accountID);
+	    }
+		
+		if( accountID == -1)
+		{
+			System.out.println("Unable to retrieve accountID...");
+		}
+		return accountID;
+	}
+	
+	//This is the method that will be called to send info to the character select screen.
+	//This method gets basic char info such as name, class, and level for each char for the account and 
+	//builds a string seperating each value for a char with a space.  Each char is seperated by a colon (ie ":").
+	public String getBasicCharsInfo(int accountID)
+	{
+		int numChars = 0;
+		
+		//start of pasted code...
+		String sql;
+		ResultSet rs;
+		String ofAllStrings = "";
+		
+		try 
+		{	
+			//Check to make sure that the account name does not already exist.
+			sql = "SELECT * from CharacterInfoTable where accountID = \'" + accountID + "\';";
+			rs = stmt.executeQuery(sql);
+	        while( rs.next() )
+			{
+				if(numChars>0)
+				{
+					ofAllStrings += ":";
+				}
+				ofAllStrings += (rs.getString("characterName") + " " + rs.getString("class") + " " + rs.getString("level"));
+				numChars++;
+			}
+			System.out.println(ofAllStrings);
+			rs.close();
+			return ofAllStrings;
+			
+		} 
+	    catch (SQLException e) {
+	       e.printStackTrace();
+	    }
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    finally
+	    {  
+	       System.out.println("getNumChars completed");
+	    }
+			
+//			rs.beforeFirst();
+//			rs.next();
+//			System.out.println("Before insert statement, numResults = " + numChars);
+//			if( numChars >= 5 ){
+//				errorCode="Maximum number of characters already reached";
+//				System.out.println(errorCode);
+//				rs.close();
+//				return errorCode;
+//			}
+		
+		//end of pasted code...
+		return ofAllStrings;
+	}
+	
+	public String getCharInfo(String accountID)
+	{
+		String charInfo = "defaultValue";  //This will be returned;
+		String charName;
+		String charLevel;
+		String charClass;
+		int numChars;
+		
+		String sql;
+		ResultSet rs;
+		
+		try 
+		{	
+			//Check to make sure that the account name does not already exist.
+			sql = "SELECT * from CharacterInfoTable where accountID = \'" + accountID + "\';";
+			rs = stmt.executeQuery(sql);
+	
+			
+			
+			
+		} 
+	    catch (SQLException e) {
+	       e.printStackTrace();
+	    }
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    finally
+	    {  
+	       System.out.println("create character attempt completed");
+	    }
+		
+		
+		
+		return charInfo;
+	}
+	
+	public String createChar(String charName, String accountID , String charClass, String gender, String str, String dex, String con, String charStatInt, String wil, String luck)
+	{
+		String errorCode = "dunnoYet";
+		int mana;
+		if( charClass.equals("Mage"))
+		{
+			mana = 20;
+		}
+		else mana = 0;
+		String sql = "INSERT INTO CharacterInfoTable (characterName, accountID, class, level, gender, health, mana, experience, xCoord, yCoord, gold, equippedItems, strength, dexterity, constitution,  intelligence, willpower, luck) "
+				+ "VALUES ( \'" + charName + "\', \'" + accountID + "\', \'" + charClass + "\', '1', \'" + gender + "\', \'" + con + "\', \'" + mana + "\', '0', '0', '0', '0', '0:0:0:0:0:0', \'" +str + "\', \'" + dex + "\', \'" + con + "\', \'" + charStatInt + "\', \'" + wil+ "\', \'" + luck + "\') ;";
+		System.out.println(sql);
+		try
+		{
+			int insertResult = stmt.executeUpdate(sql);
+			if( insertResult == 1)
+			{
+				errorCode = "Unique Char created.";
+				System.out.println(errorCode);
+				//rs.close();
+				return errorCode;
+			}
+		} 
+	    catch (SQLException e) {
+	       e.printStackTrace();
+	    }
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    finally
+	    {  
+	       System.out.println("create character attempt completed");
+	    }
+		
+		return errorCode;
+	}
+	
+	public String createAccount(String username, String password, String email, String securityQuestion1, String securityAnswer1, String securityQuestion2, String securityAnswer2)
+	{
+		String errorCode = "";
 
+		//we need to add code to prevent SQL injection attacks
+		String sql;
+		ResultSet rs;
+		
+		try {
+			
+			//Check to make sure that the account name does not already exist.
+			sql = "SELECT * from AccountTable where Username = \'" + username + "\';";
+			rs = stmt.executeQuery(sql);
+
+			int numResults = 0;
+			while( rs.next() )
+			{
+				numResults++;
+			}
+			rs.beforeFirst();
+			rs.next();
+			System.out.println("Before insert statement, numResults = " + numResults);
+			if( numResults != 0){
+				errorCode="Account already exists with Username :" + username;
+				System.out.println(errorCode);
+				rs.close();
+				return errorCode;
+			}
+			
+			sql = "INSERT INTO AccountTable (username, password, email, securityQuestion1, securityAnswer1, securityQuestion2, securityAnswer2 )  VALUES ( \'" + username + "\', \'" + password + "\', \'" + email + "\', \'" + securityQuestion1 + "\', \'" + securityAnswer1 + "\', \'" + securityQuestion2 + "\', \'" + securityAnswer2 + "\') ;";
+			int insertResult = stmt.executeUpdate(sql);
+			if( insertResult == 1)
+			{
+				errorCode = "accountCreated";
+				System.out.println(errorCode);
+				rs.close();
+				return errorCode;
+			}
+			else if( insertResult > 1)
+			{
+				errorCode = "More than one account with username :" + username + " exists.  This is WRONG.";
+				System.out.println(errorCode);
+				rs.close();
+				return errorCode;
+			}
+			else if( insertResult == 0)
+			{
+				errorCode = "Failed to create account.";
+				System.out.println(errorCode);
+				rs.close();
+				return errorCode;
+			}
+			
+			
+			//The following code checks if a username exists.
+			//
+			
+//			System.out.println("insert statement result should equal 1 but is really = " + insertResult);
+//			sql = "SELECT * from AccountTable where Username = \'" + username + "\';";
+//			rs = stmt.executeQuery(sql);
+//		     
+//			numResults = 0;
+//			while( rs.next() )
+//			{
+//				numResults++;
+//			}
+//			rs.beforeFirst();
+//			rs.next();
+//			System.out.println("numResults = " + numResults);
+
+		} 
+	    catch (SQLException e) {
+	       e.printStackTrace();
+	    }
+	    catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    finally
+	    {  
+	       System.out.println("create account attempt completed");
+	    }
+		System.out.println(errorCode + "and reached final return statement of the account creation method...  weird...");
+		return errorCode;
+	}
 }
+
+
