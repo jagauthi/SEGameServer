a5f2ce05f384945d23edf6a9ea5c905af1668fd6
jfantinel <joseph.fantinel@gmail.com>
2016-02-13 14:31:12 -0600

    Update Main.java
    aaaaaaaa again.

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,10 @@ public class Main {
         db = new DatabaseConnector();
     
         //testing code below this line...
-        //db.createAccount("otherAccountNum79","whatever", "fake@email.not", "securityQuestion1", "securityAnswer1", "securityQuestion2", "securityAnswer2");
+       //db.updateAccount("otherAccountNum79","newPass", "realemail@email.not", "securityQuestion1", "securityAnswer1", "securityQuestion2", "securityAnswer2");
         //db.getAccountID("otherAccountNum79");
-       // db.createChar("WarriorPrincess", "77", "Warrior", "Female", "30", "15", "15", "5", "5", "10");
-        
+     //db.createChar("Anone", "42", "Warrior", "Male", "38", "10", "20", "1", "1", "10");
+     //db.createChar("Arinde", "42", "Rogue", "Male", "10", "30", "20", "1", "1", "18");
+     //db.createChar("Gorski", "1", "Mage", "Male", "5", "15", "5", "35", "5", "15");
     }
 }
