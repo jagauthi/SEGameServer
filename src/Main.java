f224099b656f78dda5d006b6dd1aa000d0a7547e
jfantinel <joseph.fantinel@gmail.com>
2016-02-18 20:02:28 -0600

    Update Main.java
    updated for examples of method calls.

--- src/Main.java
+++ src/Main.java
@@ -20,13 +20,16 @@ public class Main {
      //db.createChar("Anone", "42", "Warrior", "Male", "38", "10", "20", "1", "1", "10");
      //db.createChar("Arinde", "42", "Rogue", "Male", "10", "30", "20", "1", "1", "18");
      //db.createChar("Gorski", "1", "Mage", "Male", "5", "15", "5", "35", "5", "15");
-        String equippedItems = "00:00:00-";
-        String inventorySlots[] = new String[20];
-        for(int i = 0; i < 20; i++)
-        {
-        	inventorySlots[i] = "00:00";
-        }
+//        String equippedItems = "00:00:00-";
+//        String inventorySlots[] = new String[20];
+//        for(int i = 0; i < 20; i++)
+//        {
+//        	inventorySlots[i] = "00:00";
+//        }
+//        db.unlockAccount("42");
         //db.updateCharInfo("Gilsani", 1, "Mage", "2", "Male", "20", "20", "20", "200", "20", "20", "20", "20", "20", "20", "20", "20");
         //db.updateCharInventory("Gilsani", equippedItems, inventorySlots);
+        //db.deleteCharacter("KittyPryde");
+        //db.broadcastGameChanges("Gilsani", "5", "5");
     }
 }
