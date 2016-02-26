a5aee745fb435d8117f3d151dbb535d358a108fe
jfantinel <joseph.fantinel@gmail.com>
2016-02-26 11:45:58 -0600

    Update Handler.java
    Added case for specific location info aka GETLOCALINFO

--- src/Handler.java
+++ src/Handler.java
@@ -109,10 +109,15 @@ public class Handler extends Thread {
                     	errCode = Main.db.updateCharInfo(tokens[1],tokens[2],tokens[3],tokens[4],tokens[5],tokens[6],tokens[7],tokens[8],tokens[9],tokens[10],tokens[11],tokens[12],tokens[13],tokens[14], tokens[15],tokens[16],tokens[17], tokens[18],tokens[19]);
                     	out.println(errCode + ":" + Main.db.broadcastGameChanges(tokens[1], tokens[14],tokens[15]));//sends charname, x, and y position data
                     	break;
+                    case "GETLOCALINFO":
+                    	System.out.println("calling db.getSpecificLocationInfo() method");
+                    	errCode = Main.db.getSpecificLocationInfo(tokens[1]);
+                    	out.println(errCode );
+                    	break;
                     case "GETSECURITYINFO":
                     	System.out.println("calling db.getAccountInfo() method");
                     	errCode = Main.db.getAccountInfo(tokens[1]);
-                    	out.println("securityInfo:" + errCode);
+                    	out.println(errCode);
                     	break;
                     case "LOCKACCOUNT":
                     	System.out.println("calling db.lockAccount() method");
