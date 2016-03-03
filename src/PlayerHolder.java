d3d8c8b549d5084e8b8bdcfe161f758c996e3c82
jfantinel <joseph.fantinel@gmail.com>
2016-03-03 13:54:46 -0600

    Update PlayerHolder.java

--- src/PlayerHolder.java
+++ src/PlayerHolder.java
@@ -9,18 +9,28 @@ public class PlayerHolder {
 	public int yCoord;
 	public int direction;
 	public String location;
+	public String equippedItems;
+	public String sex;
+	public String charClass;
+	public int level;
 	public String status;
 	public long millis;
+	public int initiative;
 	
-	public PlayerHolder(String name, int x, int y, int d, String loc, String state )
+	public PlayerHolder(String name, int x, int y, int d, String loc, String equippedItems, String sex, String charClass, int level, String state, int initiative )
 	{
 		this.charName = name;
 		this.xCoord = x;
 		this.yCoord = y;
 		this.direction = d;
 		this.location = loc;
+		this.equippedItems = equippedItems;
+		this.sex = sex;
+		this.charClass = charClass;
+		this.level = level;
 		this.status = state;
 		this.millis = System.currentTimeMillis();
+		this.initiative = initiative;
 	}
 
 
